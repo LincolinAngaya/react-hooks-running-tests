d624d0cb0998dc7c8ed19d145f6da09b8ca1678e
LincolinAngaya <lincolinangaya5626@gmail.com>
2022-07-12 16:12:51 +0300

    commit tree fa44aa6abb8a76090743f7b792dde61e3df7b1de

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,12 @@
 import React from "react";
-import Article from './Article'
-import Header from './Header'
+import Article from "../components/Article";
+import Header from "../components/Header";
 
 function App() {
-  return <div className="App">
-    learn react!
-    <Header/>
-    <Article />
-  </div>;
+    return <div className = "App" > learn react!
+        <Header / >
+        <Article / >
+        </div>;
 }
 
 export default App;
